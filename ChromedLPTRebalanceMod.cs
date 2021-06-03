09710e819e3cb29979ff8a20c76ada4a82bf1cc4
Reika <reikasminecraft@gmail.com>
2021-06-03 00:06:21 -0400

    Initial commit

---
+++ ChromedLPTRebalanceMod.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;  //Needed for most Unity Enginer manipulations: Vectors, GameObjects, Audio, etc.
+using System.IO;    //For data read/write methods
+using System;    //For data read/write methods
+using System.Collections.Generic;   //Working with Lists and Collections
+using System.Linq;   //More advanced manipulation of lists/collections
+using Harmony;
+using ReikaKalseki;
+
+namespace ReikaKalseki.ChromedLPTRebalance
+{
+  public class ChromedLPTRebalanceMod : FortressCraftMod
+  {
+    public const string MOD_KEY = "ReikaKalseki.ChromedLPTRebalance";
+    public const string CUBE_KEY = "ReikaKalseki.ChromedLPTRebalance_Key";
+    
+    private const float GREEN_BASE_PPS = 10;
+    private const float BLUE_BASE_PPS = 40;
+    private const float PURPLE_BASE_PPS = 320;
+    
+    private const float CHROMED_FACTOR = 5F;
+    
+    private const float CHROMED_GREEN_PPS = 180;
+    private const float CHROMED_BLUE_PPS = 250;
+    private const float CHROMED_PURPLE_PPS = 2000; //1600 in vanilla
+
+    public override ModRegistrationData Register()
+    {
+        ModRegistrationData registrationData = new ModRegistrationData();
+        //registrationData.RegisterEntityHandler(MOD_KEY);
+        /*
+        TerrainDataEntry entry;
+        TerrainDataValueEntry valueEntry;
+        TerrainData.GetCubeByKey(CUBE_KEY, out entry, out valueEntry);
+        if (entry != null)
+          ModCubeType = entry.CubeType;
+         */        
+        var harmony = HarmonyInstance.Create("ReikaKalseki.ChromedLPTRebalance");
+        HarmonyInstance.DEBUG = true;
+        FileLog.Log("Ran mod register, started harmony (harmony log)");
+        Debug.Log("Ran mod register, started harmony");
+        try {
+			harmony.PatchAll();
+        }
+        catch (Exception e) {
+			FileLog.Log("Caught exception when running patcher!");
+			FileLog.Log(e.Message);
+			FileLog.Log(e.StackTrace);
+			FileLog.Log(e.ToString());
+        }
+        
+        return registrationData;
+    }
+    
+    private static float calculateNeededBonus(float baseGen, float target) {
+    	return (target-baseGen);
+    }
+    
+    public static float getPPSBonus(float original, LaserPowerTransmitter lpt, int tier) {
+    	if (tier != 10) //not chromed
+    		return original;
+    	if (lpt.mValue > 2) //not one of the base three
+    		return original;
+    	switch(lpt.mValue) {
+    		case 0: //green
+    			return calculateNeededBonus(GREEN_BASE_PPS, CHROMED_GREEN_PPS);
+    		case 1: //blue
+    			return calculateNeededBonus(BLUE_BASE_PPS, CHROMED_BLUE_PPS);
+    		case 2: //purple
+    			return calculateNeededBonus(PURPLE_BASE_PPS, CHROMED_PURPLE_PPS);
+    		default:
+    			return original;
+    	}
+    }
+    
+    public static float getPPSMultiplier(float original, LaserPowerTransmitter lpt, int tier) {
+    	if (tier != 10) //not chromed
+    		return original;
+    	if (lpt.mValue > 2) //not one of the base three
+    		return original;
+    	return 0; //so as not to cause issues when combined with bonus
+    }
+
+  }
+}
---
+++ Patches.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Reika
+ * Date: 04/11/2019
+ * Time: 11:28 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;    //For data read/write methods
+using System.Collections;   //Working with Lists and Collections
+using System.Collections.Generic;   //Working with Lists and Collections
+using System.Linq;   //More advanced manipulation of lists/collections
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Diagnostics;
+using Harmony;
+using UnityEngine;  //Needed for most Unity Enginer manipulations: Vectors, GameObjects, Audio, etc.
+
+namespace ReikaKalseki.ChromedLPTRebalance {
+	
+	[HarmonyPatch(typeof(LaserPowerTransmitter))]
+	[HarmonyPatch("GetBonus")]
+	public static class BonusPatch {
+		
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			return Lib.patch(instructions, "getPPSBonus");
+		}
+	}
+	
+	[HarmonyPatch(typeof(LaserPowerTransmitter))]
+	[HarmonyPatch("GetMultiplier")]
+	public static class FactorPatch {
+		
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			return Lib.patch(instructions, "getPPSMultiplier");
+		}
+	}
+	
+	static class Lib {
+	
+		internal static IEnumerable<CodeInstruction> patch(IEnumerable<CodeInstruction> li, string call) {
+			List<CodeInstruction> codes = new List<CodeInstruction>(li);
+			try {
+				int loc = InstructionHandlers.getLastInstructionBefore(codes, codes.Count, OpCodes.Ret);
+				FileLog.Log("Running patch, which found anchor "+InstructionHandlers.toString(codes, loc));
+				List<CodeInstruction> inject = new List<CodeInstruction>();
+				inject.Add(new CodeInstruction(OpCodes.Ldarg_0)); //this
+				inject.Add(new CodeInstruction(OpCodes.Ldarg_1)); //int tier
+				inject.Add(InstructionHandlers.createMethodCall("ReikaKalseki.ChromedLPTRebalance.ChromedLPTRebalanceMod", call, false, typeof(float), typeof(LaserPowerTransmitter), typeof(int)));
+				FileLog.Log("Injecting "+inject.Count+" instructions: "+InstructionHandlers.toString(inject));
+				codes.InsertRange(loc, inject);
+				FileLog.Log("Done patch "+new StackFrame(1).GetMethod().Name);
+			}
+			catch (Exception e) {
+				FileLog.Log("Caught exception when running patch "+new StackFrame(1).GetMethod().Name+"!");
+				FileLog.Log(e.Message);
+				FileLog.Log(e.StackTrace);
+				FileLog.Log(e.ToString());
+			}
+			return codes.AsEnumerable();
+		}
+		
+	}
+}
